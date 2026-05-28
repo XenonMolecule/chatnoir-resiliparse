ca234c85c8cf681c72ebebd6967e15ae808255d8
Janek Bevendorff <janek@jbev.net>
2026-05-28 22:59:02 +0200

    Print byte throughput in profile binary

--- fastwarc-rs/src/bin/profile.rs
+++ fastwarc-rs/src/bin/profile.rs
@@ -8,24 +8,37 @@ fn main() {
     let start = Instant::now();
     let mut last_timer = start;
     let mut last_count = 0usize;
+    let mut last_bytes = 0u64;
     let mut total_count = 0usize;
-    println!("Reading WARC file: {}", path);
+    let mut total_bytes = 0u64;
 
     let file = std::fs::File::open(path).unwrap();
     let reader = BufReader::with_capacity(1 << 20, file);
 
+    println!("Reading WARC file: {}", path);
     for record in ArchiveIterator::new(reader) {
         if record.is_err() {
             continue;
         }
-        record.unwrap().with_mut(|_| {
+        record.unwrap().with_mut(|record| {
+            let content_length = record.content_length();
             last_count += 1;
+            last_bytes += content_length;
             total_count += 1;
+            total_bytes += content_length;
 
             let elapsed = last_timer.elapsed();
             if elapsed >= Duration::from_millis(500) {
-                println!("{:.1} records/s ({} total)", last_count as f64 / elapsed.as_secs_f64(), total_count);
+                println!(
+                    "{:.0} records/s, {:.1} MiB/s, {:.1} KiB/rec ({} total, {:.1} MiB)",
+                    last_count as f64 / elapsed.as_secs_f64(),
+                    last_bytes as f64 / elapsed.as_secs_f64() / 1024.0 / 1024.0,
+                    last_bytes as f64 / last_count.max(1) as f64 / 1024.0,
+                    total_count,
+                    total_bytes as f64 / 1024.0 / 1024.0
+                );
                 last_count = 0;
+                last_bytes = 0;
                 last_timer = Instant::now();
             }
         });
